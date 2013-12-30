aaa5acc7ff70c6603c574c0b2b0c9572fecde8eb
Markus Jung <markus.jung85@gmail.com>
2013-12-30 20:43:21 +0100

    Changed port

--- examples/iotsys/iotsys-group-communication.c
+++ examples/iotsys/iotsys-group-communication.c
@@ -43,7 +43,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define UDP_PORT 1234
+#define UDP_PORT 5683
 
 #define SEND_INTERVAL		(10 * CLOCK_SECOND)
 #define SEND_TIME		(random_rand() % (SEND_INTERVAL))
